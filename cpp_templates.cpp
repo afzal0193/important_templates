f736bc3ac40bd567e8f601421ea8f18bab86f951
Afzal khan <108002956+afzal0193@users.noreply.github.com>
2023-05-29 12:28:38 +0600

    Update cpp_templates.cpp

--- cpp_templates.cpp
+++ cpp_templates.cpp
@@ -1,8 +1,20 @@
-#include <bits/stdc++.h>
+
+
+
+/*
+
+ Afzal Khan -> CSE,BBPI
+ Praying(Everything is depend on Allah)...
+ 
+ Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem
+ Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem
+ Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,
+ Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,
+ Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem,Bismillahir Rahmanir Raheem ~~~~~ !!! 
+*/
+
+#include<bits/stdc++.h>
 using namespace std;
-
-/* clang-format off */
-
 /* TYPES  */
 #define ll long long
 #define pii pair<int, int>
@@ -12,6 +24,8 @@ using namespace std;
 #define mii map<int, int>
 #define si set<int>
 #define sc set<char>
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
 
 /* FUNCTIONS */
 #define f(i,s,e) for(long long int i=s;i<e;i++)
@@ -25,45 +39,58 @@ template <class T>
 void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
 
 /* UTILS */
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795
-#define read(type) readInt<type>()
+
 ll min(ll a,int b) { if (a<b) return a; return b; }
 ll min(int a,ll b) { if (a<b) return a; return b; }
 ll max(ll a,int b) { if (a>b) return a; return b; }
 ll max(int a,ll b) { if (a>b) return a; return b; }
 ll gcd(ll a,ll b) { if (b==0) return a; return gcd(b, a%b); }
-ll lcm(ll a,ll b) { return a/gcd(a,b)*b; }
+ll lcm(ll a,ll b) { return a/gcd(a,b)*b;}
 string to_upper(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A'; return a; }
 string to_lower(string a) { for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A'; return a; }
 bool prime(ll a) { if (a==1) return 0; for (int i=2;i<=round(sqrt(a));++i) if (a%i==0) return 0; return 1; }
 void yes() { cout<<"YES\n"; }
 void no() { cout<<"NO\n"; }
+/*---------------------------------------------------------------------------------------------------------------------------*/
+int gcd(int a, int b) {if (b > a) {return gcd(b, a);} if (b == 0) {return a;} return gcd(b, a % b);}
+int expo(int a, int b, int mod) {int res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+void extendgcd(int a, int b, int*v) {if (b == 0) {v[0] = 1; v[1] = 0; v[2] = a; return ;} extendgcd(b, a % b, v); int x = v[1]; v[1] = v[0] - v[1] * (a / b); v[0] = x; return;} //pass an arry of size1 3
+int mminv(int a, int b) {int arr[3]; extendgcd(a, b, arr); return arr[0];} //for non prime b
+int mminvprime(int a, int b) {return expo(a, b - 2, b);}
+bool revsort(int a, int b) {return a > b;}
 
-/*  All Required define Pre-Processors and typedef Constants */
+// convert string Binary to integar ....
+int bin_to_int(string value){int number =stoi(value,0,2);return number;}
+string int_to_bin(int n){string value = bitset<11>(n).to_string();int v = value.find("1");return value.substr(v,11);}
+void swap(int &x, int &y) {int temp = x; x = y; y = temp;}
+int combination(int n, int r, int m, int *fact, int *ifact) {int val1 = fact[n]; int val2 = ifact[n - r]; int val3 = ifact[r]; return (((val1 * val2) % m) * val3) % m;}
+void google(int t) {cout << "Case #" << t << ": ";}
+vector<int> sieve(int n) {int*arr = new int[n + 1](); vector<int> vect; for (int i = 2; i * i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+int mod_add(int a, int b, int m) {a = a % m; b = b % m; return (((a + b) % m) + m) % m;}
+int mod_mul(int a, int b, int m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+int mod_sub(int a, int b, int m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+int mod_div(int a, int b, int m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+int phin(int n) {int number = n; if (n % 2 == 0) {number /= 2; while (n % 2 == 0) n /= 2;} for (int i = 3; i <= sqrt(n); i += 2) {if (n % i == 0) {while (n % i == 0)n /= i; number = (number / i * (i - 1));}} if (n > 1)number = (number / n * (n - 1)) ; return number;} //O(sqrt(N))
+vector<bool> isprime(int n) { vector<bool> sieve(n + 1, true); for (int i = 2 ; i * i <= n ; i++) { if (sieve[i]) { for (int j = 2 * i ; j <= n ; j += i) { sieve[j] = false; } } } return sieve; }
+/*--------------------------------------------------------------------------------------------------------------------------*/
+
+/* All Required define Pre-Processors and typedef Constants */
 typedef long int int32;
 typedef unsigned long int uint32;
 typedef long long int int64;
 typedef unsigned long long int  uint64;
 
 
-/* clang-format on */
+/*-----------------------------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------------------------*/
 
-/* Main()  function */
-int main()
-{
+#include<bits/stdc++.h>
+using namespace std;
 
-	#ifndef ONLINE_JUDGE
-	freopen("input.txt","r",stdin);
-	//freopen("output.txt","w",stdout);
-	#endif
+int main(){
+    
 
-	int tc;
-	tc = read(int);
 
-	while(tc--){
-		write(tc);
-	}
-	return 0;
+ return 0;
 }
-/* Main() Ends Here */
+
